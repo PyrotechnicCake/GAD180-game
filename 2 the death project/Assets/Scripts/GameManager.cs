b512c4ef60dcf0b0d3935718c9d1638a0bec1770
Jake <onetruejake@outlook.com>
2019-10-08 16:27:29 +1100

    Added a game manager and some variables

---
+++ 2 the death project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //intialize a stat set for both players
+    public int hp = 5;
+    public int atk = 1;
+    public int stam = 100;
+    public int ammo = 20;
+    public int lives = 3;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
