ff0d783dc1242214b74f0fcbe691d7e571a35f0d
Jake <onetruejake@outlook.com>
2019-10-11 17:36:33 +1100

    coded more varibles and made player script
    I made a new script for the player, it collects the values of the gamemanager's variables so they can remain consistent.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,11 +7,14 @@ public class GameManager : MonoBehaviour
     //intialize a stat set for both players
     public int hp = 5;
     public int atk = 1;
+    public int atkspd = 1;
     public int stam = 100;
     public int ammo = 20;
     public int lives = 3;
-    //yabba dabba doo
-    //it doo
+
+    //movement based stats to be called when calling movement
+    public float movespeed = 5f;
+    public int dir;
 
 
     // Start is called before the first frame update
---
+++ Assets/Scripts/Playerstats.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playerstats : MonoBehaviour
+{
+    //default stats
+    public int hp;
+    public int atk;
+    public int atkspd;
+    public int stam;
+    public int ammo;
+    public int lives;
+    public float movespeed;
+    public int dir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadStats();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LoadStats()
+    {
+        hp = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>().hp;
+        atk = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>().atk;
+        stam = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>().stam;
+        ammo = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>().ammo;
+        lives = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>().lives;
+        movespeed = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>().movespeed;
+        dir = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>().dir;
+    }
+}
