49b04cd512a6ec3b909d17a02d56e29ca8148be7
Luke Stanley <1015748@student.sae.edu.au>
2019-10-21 14:44:26 +1100

    Added Movement script
    Movement basics and camera script working

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 6.0f;
+    public float jumpForce = 8.0f;
+    public float gravity = 20.0f;
+    public float rotSpeed = 90f;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(0, 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+
+        }
+
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime, 0);
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
