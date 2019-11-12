bdb905d7037b33d400bb21bb2d466fe67a888c01
Luke Stanley <1015748@student.edu.au>
2019-11-12 13:01:34 +1100

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/Shield.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+    public bool shieldUp = false;
+    public Animator shieldAnim;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        shieldAnim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            shieldAnim.SetTrigger("Defend");
+            shieldUp = true;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            shieldAnim.SetTrigger("Lower");
+            shieldUp = false;
+        }
+    }
+}
