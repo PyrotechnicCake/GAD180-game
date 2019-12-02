da4c8ff881795c01fcd14d1a5dca3b0276259208
Luke Stanley <1015748@student.sae.edu.au>
2019-12-02 14:32:53 +1100

    Added respawn point to Colosseum
    fiddled with camera manager and turned it off (not working properly)

--- Assets/Scripts/Managers/CameraManager.cs
+++ Assets/Scripts/Managers/CameraManager.cs
@@ -6,33 +6,41 @@ public class CameraManager : MonoBehaviour
 {
 
     //initial values
-    private Vector3 startingPosition;
+    public Vector3 centre;
     private Vector3 newPosition;
     [SerializeField]
     private float distBetweenPlayers;
     public float maxHeight = 20;
+    public int camHeight;
 
     private GameObject player1;
     private GameObject player2;
 
     // Start is called before the first frame update
-    void Start()
+    private IEnumerator Start()
     {
         //store initial position
-        startingPosition = transform.position;
-        player1 = GameObject.FindGameObjectWithTag("player1");
-        player2 = GameObject.FindGameObjectWithTag("player2");
-
+        yield return new WaitForSeconds(.5f);
+        Setplayers();
     }
 
+    public void Setplayers()
+    {
+        player1 = GameObject.FindGameObjectWithTag("player1");
+        player2 = GameObject.FindGameObjectWithTag("player2");
+    }
+    
     // Update is called once per frame
     void Update()
     {
+        centre = ((player1.transform.position - player2.transform.position) / 2.0f) + player1.transform.position;
         //detect the player's distance apart
         distBetweenPlayers = Vector3.Distance(player1.transform.position, player2.transform.position);
         //adjust the height
         //newPosition = transform.position;
-        newPosition.y = Mathf.Lerp(startingPosition.y, maxHeight, distBetweenPlayers / maxHeight);
-        transform.position = new Vector3(transform.position.x,newPosition.y,transform.position.z);
+        newPosition.z = Mathf.Lerp(centre.z, maxHeight, distBetweenPlayers / maxHeight);
+        newPosition.x = centre.x;
+        newPosition.y = centre.y;
+        transform.position = new Vector3(newPosition.x,newPosition.y + camHeight ,newPosition.z);
     }
 }
